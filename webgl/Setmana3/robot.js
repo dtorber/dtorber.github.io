f818f45d7a1a4f689e5a4568732cdc390b03f143
dtorber <dtorresbert@gmail.com>
2023-09-27 20:29:46 +0200

    Que se vegen les dos animacions a la vegada

--- webgl/Setmana3/robot.js
+++ webgl/Setmana3/robot.js
@@ -7,7 +7,9 @@ let renderer, scene, camera;
 
 //Otras globales
 let angulo = 0;
-const material = new THREE.MeshBasicMaterial({color:'red', wireframe: true});
+const material = new THREE.MeshNormalMaterial({flatShadig:true, wireframe: false});
+let planta; //per a vista de planta
+const L = 5;
 
 //Accions
 init();
@@ -17,11 +19,13 @@ render();
 function init() {
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0xAAAAAA);
+    renderer.autoClear = false;
     document.getElementById("container").appendChild(renderer.domElement);
 
     //Instanciar el node arrel de l'escena
     scene = new THREE.Scene();
-    scene.background = new THREE.Color(0.5, 0.5, 0.5);
+    // scene.background = new THREE.Color(0.5, 0.5, 0.5);
 
     //Instanciar la càmera
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
@@ -33,6 +37,12 @@ function init() {
     //Limitem els controls perquè no es puga acostar massa ni allunyar-se massa
     controls.minDistance = 1;
     controls.maxDistance = 1000;
+
+    //Per a que les càmeres es redimensionen amb la finestra
+    const ar = window.innerWidth / window.innerHeight;
+    setCameras(ar);
+    //Captura d'esdeveniments
+    window.addEventListener('resize', updateAspectRatio);    
 }   
 
 
@@ -56,5 +66,51 @@ function update () {
 function render() {
     requestAnimationFrame(render);
     //update();
+    renderer.clear();
+    
+    let w = window.innerWidth / 2;
+    let h = window.innerHeight / 2;
+
+    renderer.setViewport(0,0,2*w,2*h);
     renderer.render(scene, camera);
+
+    const lado = Math.min(window.innerWidth, window.innerHeight) / 4;
+    renderer.setViewport(0,lado*3,lado, lado);
+    renderer.render(scene, planta);
+}
+
+function setCameras (ar) {
+    let camaraOrto;
+    if (ar > 1) 
+        camaraOrto = new THREE.OrthographicCamera(-L*ar, L*ar, L, -L, -20, 230);
+    else
+        camaraOrto = new THREE.OrthographicCamera(-L, L, L / ar, -L / ar, -20, 230);
+
+    planta = camaraOrto.clone();
+    planta.position.set(0,230,0);
+    planta.lookAt(0,0,0);
+    planta.up = new THREE.Vector3(0,0,-1);
+}
+
+function updateAspectRatio () {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    //Nova aspect ratio
+    const ar = window.innerWidth / window.innerHeight;
+    
+    ///perspectiva
+    camera.aspect = ar;
+    camera.updateProjectionMatrix();
+    if (ar > 1) {
+        planta.left = -L * ar;
+        planta.right = L * ar;
+        planta.top = L;
+        planta.bottom = -L;
+    } else {
+        planta.left = -L;
+        planta.right = L;
+        planta.top = L / ar;
+        planta.bottom = -L / ar;
+    }
+
+    planta.updateProjectionMatrix();
 }
